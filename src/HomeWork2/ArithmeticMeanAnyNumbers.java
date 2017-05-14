cb334858f533664f3a750d6a192ad40482c8466b
Vlad Velev <velev.vlad@gmail.com>
2017-05-15 01:51:32 +0300

    Среднее арифметическое произвольного количества чисел

---
+++ src/HomeWork2/ArithmeticMeanAnyNumbers.java
@@ -0,0 +1,71 @@
+package HomeWork2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Velev Vlad on 15.05.2017.
+ */
+public class ArithmeticMeanAnyNumbers {
+    public static void main(String[] args) {
+        ArrayList <Integer> list = new ArrayList<>() ;
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        int tempNum;
+
+        System.out.println("This program Calc Arithmetic Mean of any numbers that you input");
+        System.out.println("Please input number. To the End input E.");
+
+        do {
+            input = scanner.next();
+            if( input.toLowerCase().equals(new String("e"))){
+                if(list.isEmpty()){
+                    System.out.println("You didn't input numbers");
+                    continue;
+                }
+                if (list.size() < 2) {
+                    System.out.println("Please input min 2 numbers");
+                    continue;
+
+                }
+
+
+            } else {
+                try {
+                    tempNum = Integer.parseInt(input);
+                    list.add(tempNum);
+                    continue;
+                } catch (NumberFormatException e) {
+
+                    System.out.println("You input NOT correct integer number!");
+                    System.out.println("You input: " + input);
+                    System.out.println("Please input correct number:");
+                    continue;
+                }
+            }
+
+            break;
+
+
+
+
+
+        } while(true);
+
+        int sum = 0;
+
+        for (Integer num : list){
+            sum += num;
+        }
+
+        System.out.println(" Arithmetic Mean for input numbers is " + (double)sum / list.size() );
+
+
+
+
+    }
+
+
+
+}
